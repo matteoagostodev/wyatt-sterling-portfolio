df1362d690df380fc675c328a78a6ed41689990a
matteoagostodev <matteoagosto01@icloud.com>
2025-05-22 10:13:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/wyatt-sterling-portfolio/',  // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
